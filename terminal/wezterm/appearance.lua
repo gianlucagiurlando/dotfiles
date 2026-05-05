3973c79d23b41927160ccbe7cfccaf88da021f59
Gianluca Giurlando <88482805+gianlucagiurlando@users.noreply.github.com>
2026-05-05 16:13:16 +0200

    chore: update terminal config

--- terminal/wezterm/appearance.lua
+++ terminal/wezterm/appearance.lua
@@ -1,11 +1,15 @@
 -- ============ APPEARANCE ============
+-- https://wezterm.org/colorschemes/index.html
 
 local M = {}
 
 M.config = {
 	enable_tab_bar = false,
 	window_decorations = "RESIZE",
-	color_scheme = "Dracula (Official)",
+	--color_scheme = "GruvboxDark",
+	color_scheme = "Gruvbox Dark (Gogh)",
+	--color_scheme = "Dracula (Official)",
+
 	-- window_background_opacity  = 0.8,
 	-- macos_window_background_blur = 10,
 }
