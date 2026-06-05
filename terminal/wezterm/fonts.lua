adf3e26242c34efdaaeb0120fe5cfdb4ed1dff68
Gianluca Giurlando <88482805+gianlucagiurlando@users.noreply.github.com>
2026-06-05 16:17:16 +0200

    added a few fonts for Wezterm

--- terminal/wezterm/fonts.lua
+++ terminal/wezterm/fonts.lua
@@ -5,8 +5,14 @@ local wezterm = require("wezterm")
 local M = {}
 
 M.config = {
-	font = wezterm.font("JetBrainsMono Nerd Font"),
+	font = wezterm.font("CaskaydiaMono Nerd Font"),
 	font_size = 19,
 }
 
 return M
+
+
+-- RecMonoSmCasual Nerd Font Mono
+-- JetBrainsMono Nerd Font
+-- CaskaydiaMono Nerd Font
+
