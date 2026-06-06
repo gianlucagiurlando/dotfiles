78b30371349a7d17b9ac742a658cf2801fe44370
Gianluca Giurlando <88482805+gianlucagiurlando@users.noreply.github.com>
2026-06-06 19:55:27 +0200

    added a few fonts for Wezterm

--- terminal/wezterm/fonts.lua
+++ terminal/wezterm/fonts.lua
@@ -5,14 +5,43 @@ local wezterm = require("wezterm")
 local M = {}
 
 M.config = {
-	font = wezterm.font("CaskaydiaMono Nerd Font"),
+	font = wezterm.font("OperatorMono Nerd Font", { weight = "Regular" }),
 	font_size = 19,
 }
 
 return M
 
-
+--INSTALLED FONTS:
 -- RecMonoSmCasual Nerd Font Mono
 -- JetBrainsMono Nerd Font
 -- CaskaydiaMono Nerd Font
+-- Blexmono Nerd Font
+-- Hack Nerd Font Mono
+-- MesloLGS Nerd Font Mono
 
+
+
+--A LIST OF ALL NERD FONTS INSTALLED ON MY MAC
+-- fc-list | grep "Nerd Font" | sed -E 's/.*:([^:]+):.*/\1/' | sort -u
+
+
+
+--TO CHECK INSTALLED WEIGHTS OF A FONT
+--find ~/Library/Fonts -iname '*Blex*' | sed 's|.*/||' | sort
+-- 400 = Regular
+-- 500 = Medium
+-- 600 = DemiBold
+-- 700 = Bold
+-- 800 = ExtraBold
+-- 900 = Black
+
+-- Thin
+-- ExtraLight
+-- Light
+-- Text
+-- Regular
+-- Medium
+-- SemiBold
+-- Bold
+-- ExtraBold
+-- Black
