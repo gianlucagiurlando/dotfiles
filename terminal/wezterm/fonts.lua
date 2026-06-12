f96b928903547322b720a562b2e494ea69004aa1
Gianluca Giurlando <88482805+gianlucagiurlando@users.noreply.github.com>
2026-06-12 11:38:47 +0200

    added 2 new patched font

--- terminal/wezterm/fonts.lua
+++ terminal/wezterm/fonts.lua
@@ -5,21 +5,15 @@ local wezterm = require("wezterm")
 local M = {}
 
 M.config = {
-	font = wezterm.font("MonoLisa Nerd Font", { weight = "Regular" }),
+	--font = wezterm.font("MonoLisa Nerd Font", { weight = "Regular" }),
+	--font = wezterm.font("OperatorMono Nerd Font", { weight = "Regular" }),
+	font = wezterm.font("BerkeleyMono Nerd Font", { weight = "Regular" }),
+
 	font_size = 18,
 }
 
 return M
 
---INSTALLED FONTS:
--- RecMonoSmCasual Nerd Font Mono
--- JetBrainsMono Nerd Font
--- CaskaydiaMono Nerd Font
--- Blexmono Nerd Font
--- Hack Nerd Font Mono
--- MesloLGS Nerd Font Mono
--- MonoLisa Nerd Font
---OperatorMono Nerd Font
 
 --A LIST OF ALL NERD FONTS INSTALLED ON MY MAC
 -- fc-list | grep "Nerd Font" | sed -E 's/.*:([^:]+):.*/\1/' | sort -u
