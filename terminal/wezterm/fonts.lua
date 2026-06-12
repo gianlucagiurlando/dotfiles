d37ddcb4ed7676661f20ad7407247eb9cf3761e2
Gianluca Giurlando <88482805+gianlucagiurlando@users.noreply.github.com>
2026-06-12 11:39:44 +0200

    added 2 new patched font

--- terminal/wezterm/fonts.lua
+++ terminal/wezterm/fonts.lua
@@ -5,9 +5,9 @@ local wezterm = require("wezterm")
 local M = {}
 
 M.config = {
-	--font = wezterm.font("MonoLisa Nerd Font", { weight = "Regular" }),
+	font = wezterm.font("MonoLisa Nerd Font", { weight = "Regular" }),
 	--font = wezterm.font("OperatorMono Nerd Font", { weight = "Regular" }),
-	font = wezterm.font("BerkeleyMono Nerd Font", { weight = "Regular" }),
+	--font = wezterm.font("BerkeleyMono Nerd Font", { weight = "Regular" }),
 
 	font_size = 18,
 }
