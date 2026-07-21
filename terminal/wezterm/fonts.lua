0b1a6f9e05710c42e12043fe127acf78253071b4
Gianluca Giurlando <88482805+gianlucagiurlando@users.noreply.github.com>
2026-07-21 15:52:46 +0200

    update

--- terminal/wezterm/fonts.lua
+++ terminal/wezterm/fonts.lua
@@ -5,38 +5,14 @@ local wezterm = require("wezterm")
 local M = {}
 
 M.config = {
-	font = wezterm.font("MonoLisa Nerd Font", { weight = "Regular" }),
+	--font = wezterm.font("MonoLisa Nerd Font", { weight = "Regular" }),
+	font = wezterm.font("PragmataPro Nerd Font", { weight = "Regular" }),
 	--font = wezterm.font("OperatorMono Nerd Font", { weight = "Regular" }),
 	--font = wezterm.font("BerkeleyMono Nerd Font", { weight = "Regular" }),
-	font_size = 18,
+	font_size = 20,
 	--cursor's shape
 	default_cursor_style = "SteadyBar",
 }
 
 return M
 
-
---A LIST OF ALL NERD FONTS INSTALLED ON MY MAC
--- fc-list | grep "Nerd Font" | sed -E 's/.*:([^:]+):.*/\1/' | sort -u
-
-
-
---TO CHECK INSTALLED WEIGHTS OF A FONT
---find ~/Library/Fonts -iname '*Blex*' | sed 's|.*/||' | sort
--- 400 = Regular
--- 500 = Medium
--- 600 = DemiBold
--- 700 = Bold
--- 800 = ExtraBold
--- 900 = Black
-
--- Thin
--- ExtraLight
--- Light
--- Text
--- Regular
--- Medium
--- SemiBold
--- Bold
--- ExtraBold
--- Black
