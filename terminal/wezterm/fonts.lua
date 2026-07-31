5449aaadc68b912ff7d76e9e8d90d36188145f6e
Gianluca Giurlando <88482805+gianlucagiurlando@users.noreply.github.com>
2026-07-31 13:22:09 +0200

    Refine Starship prompt: modified file count, python virtualenv, cmd_duration

--- terminal/wezterm/fonts.lua
+++ terminal/wezterm/fonts.lua
@@ -5,11 +5,11 @@ local wezterm = require("wezterm")
 local M = {}
 
 M.config = {
-	--font = wezterm.font("MonoLisa Nerd Font", { weight = "Regular" }),
-	font = wezterm.font("PragmataPro Nerd Font", { weight = "Regular" }),
+	font = wezterm.font("MonoLisa Nerd Font", { weight = "Regular" }),
+	--font = wezterm.font("PragmataPro Nerd Font", { weight = "Regular" }),
 	--font = wezterm.font("OperatorMono Nerd Font", { weight = "Regular" }),
 	--font = wezterm.font("BerkeleyMono Nerd Font", { weight = "Regular" }),
-	font_size = 20,
+	font_size = 19,
 	--cursor's shape
 	default_cursor_style = "SteadyBar",
 }
