0e61c0ef8790b3cbb99320f73be686086b0ed91c
Gianluca Giurlando <88482805+gianlucagiurlando@users.noreply.github.com>
2026-08-01 16:50:26 +0200

    restructured the startship prompt

--- terminal/wezterm/fonts.lua
+++ terminal/wezterm/fonts.lua
@@ -5,11 +5,12 @@ local wezterm = require("wezterm")
 local M = {}
 
 M.config = {
-	font = wezterm.font("MonoLisa Nerd Font", { weight = "Regular" }),
+	--font = wezterm.font("MonoLisa Nerd Font", { weight = "Regular" }),
+	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" }),
 	--font = wezterm.font("PragmataPro Nerd Font", { weight = "Regular" }),
 	--font = wezterm.font("OperatorMono Nerd Font", { weight = "Regular" }),
 	--font = wezterm.font("BerkeleyMono Nerd Font", { weight = "Regular" }),
-	font_size = 19,
+	font_size = 18,
 	--cursor's shape
 	default_cursor_style = "SteadyBar",
 }
